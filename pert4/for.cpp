e71364616e71e88d8927c46157b390fcd8ef7491
desales24 <fransiskusdes24@gmail.com>
2024-06-13 19:17:49 +0700

    pert4

---
+++ pert4/for.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Iteration Statement" << endl;
+    cout << "Penggunaan FOR" << endl;
+
+    // apa beda nya int di luar for dengan int didalam for ??
+    for(int i; i<=5; i++){
+        cout << i << endl;
+    }
+    return 0;
+}
