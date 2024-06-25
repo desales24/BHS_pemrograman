2d34b7e427f813e303cda9ad4d79ba15a01baaf3
desales24 <fransiskusdes24@gmail.com>
2024-06-25 18:40:07 +0700

    pert11

--- pert11/user_service/app/api/db.py
+++ pert11/user_service/app/api/db.py
@@ -1,2 +1,2 @@
 from pymongo import MongoClient
-conn = MongoClient('mongodb://root:p455w0rd@192.168.100.3:27017/users?authSource=admin')
+conn = MongoClient('mongodb://root:p455w0rd@172.23.144.1:27017/users?authSource=admin')
